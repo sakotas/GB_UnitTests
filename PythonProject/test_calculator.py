dfed33c2e3ff2d2125ee231bc7868c2c0bc45f12
Alex Kotishevskii <kotishevskiyaa@gmail.com>
2023-11-25 16:20:57 +0300

    Lection 1 homework

--- PythonProject/test_calculator.py
+++ PythonProject/test_calculator.py
@@ -1,21 +1,29 @@
+"""
+Модуль для тестирования функциональности калькулятора, включающего сложение, 
+вычитание, умножение и деление.
+"""
+
 import pytest
-from Calculator import add, subtract, multiply, divide
+from calculator import add, subtract, multiply, divide
 
 def test_add():
+    """Тест функции сложения."""
     assert add(1, 2) == 3
     assert add(5.2, 3.3) == 8.5
 
 def test_subtract():
+    """Тест функции вычитания."""
     assert subtract(10, 5) == 5
     assert subtract(2.3, 1.1) == 1.2
 
-
 def test_multiply():
+    """Тест функции умножения."""
     assert multiply(3, 3) == 9
     assert multiply(7, -1) == -7
     assert multiply(0, 3) == 0
 
 def test_divide():
+    """Тест функции деления."""
     assert divide(10, 2) == 5
     assert divide(5, 2) == 2.5
     # Проверка исключения при делении на ноль
